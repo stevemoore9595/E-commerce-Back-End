c5510fc7c6f20bf8a09e7b79eac0c2c6ad3ca80e
stevemoore9595 <stevemoore9595@gmail.com>
2023-04-03 15:54:08 -0600

    Added code all the models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag)
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product)
 module.exports = {
   Product,
   Category,
